51df1c9c8aa15fbed9dadaca7d9ca7a1af0278a4
Santiago Martínez <ssantiago.mc@outlook.com>
2022-01-12 15:11:08 -0500

    LQR simulation completed

--- 5- Simulation/TweeprConfig.m
+++ 5- Simulation/TweeprConfig.m
@@ -3,10 +3,10 @@
 %--------------------------------------------------------------------------
 
 %% Model parameters
-mb = 0.710;     % Mass of base              [g]
-mp = 0.157;     % Mass of upper body        [g]
+mb = 0.710;     % Mass of base              [kg]
+mp = 0.157;     % Mass of upper body        [kg]
 g = 9.81;       % Earth gravity             [m/s^2]
-L = 0.145;      % Length of the pendulum    [mm]
+L = 0.145;      % Length of the pendulum    [m]
 d1 = 0.01;      % Damping of displacement   []
 d2 = 0.01;      % Damping of joint          []
 
@@ -24,17 +24,20 @@ C = [1; 0; 0; 0];       % q1 as unique output
 D = 0;                  % To complete matrix system
 
 
-%% System repreentation
+%% System representation
 sys = ss(A, B, C', D);
-% pole(sys)
+pole(sys)
 % rlocus(sys);
 
-%% LQ Regulator Controller
-% Q = eye(4);
-Q = [1     0     0     0
-     0     1     0     0
-     0     0     1     0
-     0     0     0     0];
-R = 1;
-K = lqr(A, B, Q, R);
+                                % Initial system condition
+x0 = [0; 5*(pi/180); 0; 0];     % Pos:0m - Ang:5°
+                                % Vel:0m/s - AngVel:0rad/s
+                                
+%% Space-State feedback controller
+desirePoles = [-3; -3; -3; -3]; % New desire poles of the system
+K = acker(A, B, desirePoles);   % Gain vector controller using Ackermann's
 
+%% LQ Regulator Controller
+Q = 10*eye(4);                  % States weights
+R = 1;                          % Control input weights
+K = lqr(A, B, Q, R);            % Optimal gain vector
